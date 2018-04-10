1edd5fe50baf0db429de17ac56f9d3a256dff202
Densi27 <d.desery@me.com>
2018-04-11 00:03:32 +0200

    Play with microseconds

--- src/main/java/com/github/densi27/hellojava/helloJava.java
+++ src/main/java/com/github/densi27/hellojava/helloJava.java
@@ -1,9 +1,41 @@
 package com.github.densi27.hellojava;
 
+import java.text.SimpleDateFormat;
+import java.time.*;
+
 public class helloJava {
+
+    /**
+     * Class to generate timestamps with microsecond precision
+     * For example: MicroTimestamp.INSTANCE.get() = "2012-10-21 19:13:45.267128"
+     */
+    public enum MicroTimestamp
+    {  INSTANCE ;
+
+        private long              startDate ;
+        private long              startNanoseconds ;
+        private SimpleDateFormat dateFormat ;
+
+        private MicroTimestamp()
+        {  this.startDate = System.currentTimeMillis() ;
+            this.startNanoseconds = System.nanoTime() ;
+            this.dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS") ;
+        }
+
+        public String get()
+        {  long microSeconds = (System.nanoTime() - this.startNanoseconds) / 1000 ;
+            long date = this.startDate + (microSeconds/1000) ;
+            return this.dateFormat.format(date) + String.format("%03d", microSeconds % 1000) ;
+        }
+    }
 
     public static void main(String[] args){
 
-        System.out.println("hello java");
+        String s = MicroTimestamp.INSTANCE.get();
+        LocalDateTime ldt = LocalDateTime.parse(s);
+        System.out.println(ldt);
+
     }
 }
+
+
